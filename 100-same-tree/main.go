87e0ee796d8f7f2ff38a6e53dc49ceb5b69559f9
Matthew Grosvenor <mgrosven@gmail.com>
2025-01-09 12:51:52 -0500

    Same Tree

---
+++ 100-same-tree/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "math"
+
+func main() {
+
+	// l1 := makeList([]int{1, 2, 3, 4})
+
+	isSameTree(&TreeNode{}, &TreeNode{})
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p != nil && q != nil {
+		return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return p == nil && q == nil
+}
