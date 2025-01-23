5b4395f4813b3b2a1663984d6dca5c57f962bddb
Matthew Grosvenor <mgrosven@gmail.com>
2025-01-23 12:57:36 -0500

    Task Scheduler - Greedy

--- 621-task-scheduler/main.go
+++ 621-task-scheduler/main.go
@@ -1,86 +1,40 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
 )
 
 func main() {
 
-	taskList := []byte{'A', 'A', 'A', 'B', 'B', 'B'}
-	n := 2
+	taskList := []byte{'A', 'C', 'A', 'B', 'D', 'B'}
+	n := 1
 	fmt.Println(leastInterval(taskList, n))
 }
 
+// (max - 1) * (n + 1) + numMaxCount
 func leastInterval(tasks []byte, n int) int {
 	if n == 0 {
 		return len(tasks)
 	}
-	m := make(map[byte]int)
 
-	for _, t := range tasks {
-		m[t]++
+	cnt := make([]int, 26)
+	for _, task := range tasks {
+		cnt[task-'A']++
 	}
 
-	h := &TaskHeap{}
-	for k, v := range m {
-		t := Task{
-			Name:  k,
-			Count: v,
-		}
-		h.Push(t)
-	}
-	heap.Init(h)
-
-	time := 0
-	for h.Len() > 0 {
-		temp := []Task{}
-		for i := 0; i < n+1; i++ {
-			t := heap.Pop(h).(Task)
-			if t.Count > 1 {
-				t.Count--
-				temp = append(temp, t)
-			}
-			time++
-			if h.Len() == 0 && len(temp) == 0 {
-				break
-			}
-		}
-
-		for _, t := range temp {
-			heap.Push(h, t)
+	max, numMax := 0, 0
+	for _, c := range cnt {
+		if c > max {
+			max = c
+			numMax = 1
+		} else if c == max {
+			numMax++
 		}
 	}
-	return time
-}
-
-type Task struct {
-	Name  byte
-	Count int
-	Cycle int
-}
-
-type TaskHeap []Task
-
-func (h TaskHeap) Len() int           { return len(h) }
-func (h TaskHeap) Less(i, j int) bool { return h[i].Count > h[j].Count }
-func (h TaskHeap) Swap(i, j int) {
-	if h.Len() > 1 {
-		h[i], h[j] = h[j], h[i]
+	res := (n+1)*(max-1) + numMax
+	if res > len(tasks) {
+		return res
+	} else {
+		return len(tasks)
 	}
-}
-
-func (h *TaskHeap) Push(x interface{}) {
-	*h = append(*h, x.(Task))
-}
-
-func (h *TaskHeap) Pop() interface{} {
-	if h.Len() > 0 {
-		current := *h
-		n := len(current)
-		x := current[n-1]
-		*h = current[:n-1]
-		return x
-	}
-	return Task{}
 }
